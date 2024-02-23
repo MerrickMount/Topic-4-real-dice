3c9c3f8a2b40d95cc53b7f3b7452dfd31e19eb45
Merrick Mount <339474587@GoTVDSB.ca>
2024-02-23 14:42:01 -0500

    added dice but they are angy

--- Program.cs
+++ Program.cs
@@ -11,54 +11,55 @@
             die2 = rnd.Next(1, 7);
             Console.WriteLine("This Program rolls 2 dice for you (Press Enter to continue)");
             Console.ReadLine();
+            Console.WriteLine($"{die1},  {die2}");
             if (die1 == 1)
             {
-                Console.WriteLine();
+                Console.WriteLine(" ____________\r\n|\t  \t|\r\n|\t  \t|\r\n|\t()\t|\r\n|\t  \t|\r\n| ___________ |\r\n");
             }
             if (die1 == 2)
             {
-
+                Console.WriteLine(" ____________\r\n|                        |\r\n|     ()                 |\r\n|                        |\r\n|                 ()     |\r\n| ___________ |\r\n");
             }
             if (die1 == 3)
             {
-
+                Console.WriteLine(" ____________\r\n|                        |\r\n|     ()                 |\r\n|           ()           |\r\n|                 ()     |\r\n| ___________ |\r\n");
             }
             if (die1 == 4)
             {
-
+                Console.WriteLine(" ____________\r\n|                        |\r\n|     ()         ()     |\r\n|                        |\r\n|     ()         ()     |\r\n| ___________ |\r\n");
             }
             if (die1 == 5)
             {
-
+                Console.WriteLine(" ____________\r\n|                        |\r\n|     ()         ()     |\r\n|           ()          |\r\n|     ()         ()     |\r\n| ___________ |\r\n");
             }
             if (die1 == 6)
             {
-
+                Console.WriteLine(" ____________\r\n|                        |\r\n|     ()         ()     |\r\n|     ()         ()     |\r\n|     ()         ()     |\r\n| ___________ |\r\n");
             }
 
             if (die2 == 1)
             {
-
+                Console.WriteLine(" ____________\r\n|\t  \t|\r\n|\t  \t|\r\n|\t()\t|\r\n|\t  \t|\r\n| ___________ |\r\n");
             }
             if (die2 == 2)
             {
-
+                Console.WriteLine(" ____________\r\n|                        |\r\n|     ()                 |\r\n|                        |\r\n|                 ()     |\r\n| ___________ |\r\n") ;
             }
             if (die2 == 3)
             {
-
+                Console.WriteLine(" ____________\r\n|                        |\r\n|     ()                 |\r\n|           ()           |\r\n|                 ()     |\r\n| ___________ |\r\n") ;
             }
             if (die2 == 4)
             {
-
+                Console.WriteLine(" ____________\r\n|                        |\r\n|     ()         ()     |\r\n|                        |\r\n|     ()         ()     |\r\n| ___________ |\r\n") ;
             }
             if (die2 == 5)
             {
-
+                Console.WriteLine(" ____________\r\n|                        |\r\n|     ()         ()     |\r\n|           ()          |\r\n|     ()         ()     |\r\n| ___________ |\r\n");
             }
             if (die2 == 6)
             {
-
+                Console.WriteLine(" ____________\r\n|                        |\r\n|     ()         ()     |\r\n|     ()         ()     |\r\n|     ()         ()     |\r\n| ___________ |\r\n");
             }
 
         }
