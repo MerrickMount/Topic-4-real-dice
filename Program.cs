0e59be1e81a1049ec62063aa2d5abbbacafb6821
339474587 <339474587@2260I-L29-003>
2024-02-23 14:06:14 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Topic_4_real_dice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int die1, die2;
+            Random rnd = new Random();
+            die1 = rnd.Next(1, 7);
+            die2 = rnd.Next(1, 7);
+            Console.WriteLine("This Program rolls 2 dice for you (Press Enter to continue)");
+            Console.ReadLine();
+            if (die1 == 1)
+            {
+                Console.WriteLine();
+            }
+            if (die1 == 2)
+            {
+
+            }
+            if (die1 == 3)
+            {
+
+            }
+            if (die1 == 4)
+            {
+
+            }
+            if (die1 == 5)
+            {
+
+            }
+            if (die1 == 6)
+            {
+
+            }
+
+            if (die2 == 1)
+            {
+
+            }
+            if (die2 == 2)
+            {
+
+            }
+            if (die2 == 3)
+            {
+
+            }
+            if (die2 == 4)
+            {
+
+            }
+            if (die2 == 5)
+            {
+
+            }
+            if (die2 == 6)
+            {
+
+            }
+
+        }
+    }
+}
